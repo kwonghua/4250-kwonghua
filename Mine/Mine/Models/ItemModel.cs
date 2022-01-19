24189e9ae05c3cec77ffcfbcda6fef745c527821
kwonghua <61091213+kwonghua@users.noreply.github.com>
2022-01-18 17:40:36 -0800

    Added Value attribute to  ItemModel.cs Added Value attribute to ItemModel

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -2,10 +2,13 @@
 
 namespace Mine.Models
 {
+    
     public class ItemModel
     {
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+        // the value of the item +9 damage
+        public int value { get; set; } = 0;
     }
 }
