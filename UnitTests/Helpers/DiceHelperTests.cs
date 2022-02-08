8627b90568778fc87d3e17f34c6680e09d31e9c4
kwonghua <61091213+kwonghua@users.noreply.github.com>
2022-02-07 22:33:53 -0800

    Added RollDice invalid Roll Zero to DiceHelperUnitTests.cs

---
+++ UnitTests/Helpers/DiceHelperTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mine.Helpers;
+using NUnit.Framework;
+using Mine.Models;
+
+namespace UnitTests.Helpers
+{   
+    [TestFixture]
+    public class DiceHelperTests
+    {
+        [Test]
+        public void RollDice_Invalid_Roll_Zero_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(0, 1);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(0, result);
+
+        }
+
+    }
+}
