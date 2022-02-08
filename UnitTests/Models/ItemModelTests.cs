8262cf1fc692ef9b4106fb268393cb4789f0b50c
kwonghua <61091213+kwonghua@users.noreply.github.com>
2022-02-07 21:54:37 -0800

    Added ItemModelTests.cs to the project

---
+++ UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class ItemModelTests
+    {
+
+        [Test]
+        public void ItemModel_Constructor_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            //Act
+
+            //Assert
+
+            Assert.Pass();
+        }
+    }
+}
