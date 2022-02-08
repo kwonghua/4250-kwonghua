db9b948f2e89add66b7738ad9de11509ed6f6c38
kwonghua <61091213+kwonghua@users.noreply.github.com>
2022-02-07 22:01:44 -0800

    Added ItemModel_Get UT to ItemModelTests.cs

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -40,5 +40,16 @@ namespace UnitTests.Models
             Assert.AreEqual("Text", result.Text);
             Assert.AreEqual(1, result.Value);
         }
+        [Test]
+        public void ItemModel_Get_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ItemModel();
+
+            // Assert
+            Assert.AreEqual(0, result.Value);
+        }
     }
 }
